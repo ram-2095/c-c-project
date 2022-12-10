14e863c1d6efd6b8b072dffa5b5a7fc6fecd9462
SOLANKI RAM <113303038+ram-2095@users.noreply.github.com>
2022-12-10 20:45:49 +0530

    Add files via upload

---
+++ ifElse.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+
+    int age;
+
+    cout<<"Enter your age"<<endl;
+    cin>>age;
+    if (age<1)
+    {
+        cout<<"Invalide syntax :("<<endl;
+    }
+    
+    if (age>=70)
+    {
+        cout<<"You cannot apply for FD :("<<endl;
+    if (age>150)
+    {
+        cout<<"Invalid syntex";
+    }
+    
+    }
+    
+    else (age<70 && age>1);
+    {
+        cout<<"You should apply for FD for your future :)"<<endl ;
+    }
+
+    return 0;
+}
